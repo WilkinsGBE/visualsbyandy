fb5d6c706f500fd13512ba09a8cad0d64e3a4d65
WilkinsGBE <saintilwilkins@gmail.com>
2025-03-24 21:37:57 -0400

    fix vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/visualsbyandy/',
+  base: '/', // root for Vercel
   plugins: [react()],
 });
