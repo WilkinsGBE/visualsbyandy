c46cbdf5732cd8ae85d94597b5c18b7e4cf371ee
WilkinsGBE <saintilwilkins@gmail.com>
2025-03-24 21:30:24 -0400

    correct the name of slideshow export in homepage

--- src/(tabs)/homepage.jsx
+++ src/(tabs)/homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Slideshow from "../../components/SlideShow";
+import Slideshow from "../../components/Slideshow";
 
 const Home = () => {
   return (
